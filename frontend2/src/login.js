5a8ac3fce1c246e64fff204d2edac2cb988f67a1
Shivani Bajpai <shivanibajpai542@gmail.com>
2026-06-05 11:01:53 +0530

    Update login API endpoint to production URL

--- frontend2/src/login.js
+++ frontend2/src/login.js
@@ -7,7 +7,7 @@ function Login({ onLogin, onRegister }) {
 
   const handleLogin = async () => {
     try {
-      const res = await fetch("http://localhost:5000/auth/login", {
+      const res = await fetch("https://rozgar-mitra.onrender.com/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
