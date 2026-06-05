58f1a0e6697554c177ad744c7d6a935995672b4d
Shivani Bajpai <shivanibajpai542@gmail.com>
2026-06-05 17:26:34 +0530

    Refactor webhook.js to use ES6 imports

--- Backend/routes/webhook.js
+++ Backend/routes/webhook.js
@@ -1,6 +1,17 @@
-router.post('/', async (req, res) => {
+import express from "express";
+import twilio from "twilio";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  console.log("🔥 WEBHOOK HIT");
+  console.log(req.body);
+
   const twiml = new twilio.twiml.MessagingResponse();
   twiml.message("Rozgar Mitra working ✅");
 
-  res.type('text/xml').send(twiml.toString());
+  res.type("text/xml");
+  res.send(twiml.toString());
 });
+
+export default router;
